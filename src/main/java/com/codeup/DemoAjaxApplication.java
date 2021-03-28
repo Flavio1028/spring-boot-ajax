298c5ba77247fa75c519bf5f5da9e8be99c6c73d
Flávio Rocha <flavio-10_28@hotmail.com>
2021-03-28 10:28:27 -0300

    Criação da recuperação via meta tag

--- src/main/java/com/codeup/DemoAjaxApplication.java
+++ src/main/java/com/codeup/DemoAjaxApplication.java
@@ -1,13 +1,29 @@
 package com.codeup;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.codeup.domain.SocialMetaTag;
+import com.codeup.service.SocialMetaTagService;
+
 @SpringBootApplication
-public class DemoAjaxApplication {
+public class DemoAjaxApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAjaxApplication.class, args);
+	}
+
+	@Autowired
+	SocialMetaTagService service;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		SocialMetaTag og = service.getOpenGraphByUrl("https://www.udemy.com/course/writing-clean-code/");
+		System.out.println(og.toString());
 	}
 
 }
--- src/main/java/com/codeup/domain/SocialMetaTag.java
+++ src/main/java/com/codeup/domain/SocialMetaTag.java
@@ -1,4 +1,4 @@
-package com.codeup;
+package com.codeup.domain;
 
 import java.io.Serializable;
 
@@ -41,6 +41,11 @@ public class SocialMetaTag implements Serializable {
 
 	public void setImage(String image) {
 		this.image = image;
+	}
+
+	@Override
+	public String toString() {
+		return "SocialMetaTag [site=" + site + ", title=" + title + ", url=" + url + ", image=" + image + "]";
 	}
 
 }
