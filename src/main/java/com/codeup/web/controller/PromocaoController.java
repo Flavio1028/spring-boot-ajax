5787409c17cf78a6e44c1fdcd4802e126576a7a9
Flávio Rocha <flavio-10_28@hotmail.com>
2021-04-03 12:33:34 -0300

    Validação dos campos no backend

--- src/main/java/com/codeup/web/controller/PromocaoController.java
+++ src/main/java/com/codeup/web/controller/PromocaoController.java
@@ -1,13 +1,19 @@
 package com.codeup.web.controller;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +37,15 @@ public class PromocaoController {
 	private CategoriaRepository categoriaRepository;
 
 	@PostMapping("/save")
-	public ResponseEntity<Promocao> salvarPromocao(Promocao promocao) {
+	public ResponseEntity<?> salvarPromocao(@Valid Promocao promocao, BindingResult result) {
+
+		if (result.hasErrors()) {
+			Map<String, String> erros = new HashMap<>();
+			for (FieldError error : result.getFieldErrors()) {
+				erros.put(error.getField(), error.getDefaultMessage());
+			}
+			return ResponseEntity.unprocessableEntity().body(erros);
+		}
 
 		log.info("Promocao {}", promocao.toString());
 		promocao.setDtCadastro(LocalDateTime.now());
