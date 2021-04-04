9786715fd22cade446d1de481a43395df4672c60
Flávio Rocha <flavio-10_28@hotmail.com>
2021-04-04 12:19:58 -0300

    Criação da listagem das promoções

--- src/main/java/com/codeup/web/controller/PromocaoController.java
+++ src/main/java/com/codeup/web/controller/PromocaoController.java
@@ -10,14 +10,18 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.codeup.domain.Categoria;
 import com.codeup.domain.Promocao;
@@ -35,6 +39,22 @@ public class PromocaoController {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@GetMapping("/list")
+	public String listarOfertas(ModelMap model) {
+		Sort sort = Sort.by(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);
+		model.addAttribute("promocoes", this.promocaoRepository.findAll(pageRequest));
+		return "promo-list";
+	}
+
+	@GetMapping("/list/ajax")
+	public String listarCards(ModelMap model, @RequestParam(name = "page", defaultValue = "1") int page) {
+		Sort sort = Sort.by(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(page, 8, sort);
+		model.addAttribute("promocoes", this.promocaoRepository.findAll(pageRequest));
+		return "promo-card";
+	}
 
 	@PostMapping("/save")
 	public ResponseEntity<?> salvarPromocao(@Valid Promocao promocao, BindingResult result) {
