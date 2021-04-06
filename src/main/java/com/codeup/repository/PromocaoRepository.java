9dd0d184700b8ff718dc51ed5352844f008e9ef2
Flávio Rocha <flavio-10_28@hotmail.com>
2021-04-06 12:00:24 -0300

    Criação da funcionalidade de LIKES das promoções

--- src/main/java/com/codeup/repository/PromocaoRepository.java
+++ src/main/java/com/codeup/repository/PromocaoRepository.java
@@ -1,11 +1,23 @@
 package com.codeup.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.codeup.domain.Promocao;
 
 @Repository
 public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+
+	@Transactional(readOnly = false)
+	@Modifying
+	@Query("UPDATE Promocao p SET p.likes = p.likes + 1 WHERE p.id = :id")
+	public void updateSomarLikes(@Param("id") Long id);
+
+	@Query("SELECT p.likes FROM Promocao p WHERE p.id = :id")
+	public int findLikesById(@Param("id") Long id);
 
 }
